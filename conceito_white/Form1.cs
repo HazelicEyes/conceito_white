7ce02a23ea8adf01e5a7f820d4053e3c1d2b21e8
HazelicEyes <henriquepromail9863@gmail.com>
2020-08-18 03:57:36 -0300

    Add project files.

---
+++ conceito_white/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace conceito_white
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTeste_Click(object sender, EventArgs e)
+        {
+            int c = 0;
+            while (c < 10000)
+            {
+                c = c + 1;
+                lblNumero.Text = Convert.ToString(c);
+                lblNumero.Refresh();
+
+                /* declaraçãop de variavel c, iniciada com 0.
+                 * condicao: eqnauto c for menor que 10000 sera executada o laço
+                 * incremento
+                 * refresh é utliziado para que haja variacao dos numeros */
+
+            }
+        }
+    }
+}
